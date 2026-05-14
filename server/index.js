c290240a7789b73bb3feb5527a456ffe285d7bc9
Rodjon <rshipko147@gmail.com>
2026-05-14 21:38:08 +0800

    init project

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = 5000
+
+app.get("/", (req,res) => {
+    res.json({ message: "Server is working"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`)
+})
