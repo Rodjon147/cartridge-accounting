affa5cd14a92b5ecc477cc4e86c8535a98d0bc1e
Rodjon <rshipko147@gmail.com>
2026-05-18 18:04:04 +0800

    create movement history controller

---
+++ server/controllers/movementController.js
@@ -0,0 +1,24 @@
+const db = require("../config/db")
+
+exports.getAllMovements = (req, res) => {
+  const sql = "SELECT movements.*, cartridges.model AS cartridge_model FROM movements JOIN cartridges ON movements.cartridge_id = cartridges.id ORDER BY movement_date DESC"
+
+  db.query(sql, (err, result) => {
+    if (err) res.status(500).json(err)
+    res.json(result)
+  })
+}
+
+exports.deleteMovement = (req, res) => {
+  const { id } = req.params
+
+  const sql = "DELETE FROM movements WHERE id = ?"
+
+  db.query(sql, [id], (err, result) => {
+    if (err) res.status(500).json(err)
+
+    res.json({
+      message: "Movement deleted",
+    })
+  })
+}
--- server/routes/cartridgeRoutes.js
+++ server/routes/cartridgeRoutes.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
 const {
-    getAllCartridges,
-    getCartridgeById,
-    createCartridge,
-    updateCartridge,
-    deleteCartridge
-} = require("../controllers/cartridgeController");
+  getAllCartridges,
+  getCartridgeById,
+  createCartridge,
+  updateCartridge,
+  deleteCartridge,
+} = require("../controllers/cartridgeController")
 
-router.get("/", getAllCartridges);
-router.get("/:id", getCartridgeById);
-router.post("/", createCartridge);
-router.put("/:id", updateCartridge);
-router.delete("/:id", deleteCartridge);
+router.get("/", getAllCartridges)
+router.get("/:id", getCartridgeById)
+router.post("/", createCartridge)
+router.put("/:id", updateCartridge)
+router.delete("/:id", deleteCartridge)
 
-module.exports = router;
+module.exports = router
