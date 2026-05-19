784dca9dbca6fde9462576b387d9f2375e43a9a7
Rodjon <rshipko147@gmail.com>
2026-05-19 18:55:26 +0800

    connect create movement route

--- server/controllers/movementController.js
+++ server/controllers/movementController.js
@@ -1,11 +1,26 @@
 const db = require("../database")
 
 exports.getAllMovements = (req, res) => {
-  const sql = "SELECT movements.*, cartridges.model AS cartridge_model FROM movements JOIN cartridges ON movements.cartridge_id = cartridges.id ORDER BY movement_date DESC"
+  const sql =
+    "SELECT movements.*, cartridges.model AS cartridge_model FROM movements JOIN cartridges ON movements.cartridge_id = cartridges.id ORDER BY movement_date DESC"
 
   db.query(sql, (err, result) => {
     if (err) res.status(500).json(err)
     res.json(result)
+  })
+}
+
+exports.createMovement = (req, res) => {
+  const { cartridge_id, action_type, to_location, quantity, comment } = req.body
+
+  const sql = "INSERT INTO movements (cartridge_id,action_type,to_location,quantity,comment) VALUES (?, ?, ?, ?, ?)"
+
+  db.query(sql, [cartridge_id, action_type, to_location, quantity, comment], (err, result) => {
+    if (err) res.status(500).json(err)
+
+    res.status(201).json({
+      message: "Movement created",
+    })
   })
 }
 
--- server/routes/movementRoutes.js
+++ server/routes/movementRoutes.js
@@ -2,10 +2,10 @@ const express = require("express")
 
 const router = express.Router()
 
-const { getAllMovements, deleteMovement } = require("../controllers/movementController")
+const { getAllMovements, createMovement, deleteMovement } = require("../controllers/movementController")
 
 router.get("/", getAllMovements)
-
+router.post("/", createMovement)
 router.delete("/:id", deleteMovement)
 
 module.exports = router
